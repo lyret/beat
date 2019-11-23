76309280e1e791d2a36eef9598603825429c98e6
lyret <viktor@lyresten.se>
2019-11-23 10:57:45 +0100

    adds voice interface idea sketch

---
+++ documentation/more-ideas/voiceinterface.ts
@@ -0,0 +1,22 @@
+// interface Input {
+// 	wavelengths : Map<string, Wavelength>
+// 	keyword: string
+// }
+
+
+// function beatVoiceTransform(input: Input) {
+	
+	
+// }
+
+// const interpretation =
+// 'good'
+// 'great
+// 'fantastic'
+// 'bad'
+// 'worse'
+// 'terrible'
+// |'forbidden'
+// |'perform'
+// |'learn'
+// |'forget'
